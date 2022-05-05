e6a55c775b6fc39a8568f276fb89ef6f8033c848
LeeJx2 <46637231+LeeJx12@users.noreply.github.com>
2022-05-05 11:00:51 +0900

    Feat: root 추가
    root 추가

---
+++ src/app/components/App.js
@@ -0,0 +1,79 @@
+import { Component, Fragment } from "react";
+import { Provider } from "react-redux";
+import { combineReducers, createStore } from 'redux';
+import Thunk from 'redux-thunk';
+import PersistenceRegistry from '../../redux/PersistenceRegistry';
+import MiddlewareRegistry from '../../redux/MiddlewareRegistry';
+import StateListenerRegistry from '../../redux/StateListenerRegistry';
+import ReducerRegistry from '../../redux/ReducerRegistry';
+
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            route: {},
+            store: undefined
+        }
+    }
+
+    /**
+     * 초기화
+     */
+    componentDidMount() {
+        this._init = new Promise(resolve => {
+            this.setState({
+                store: this._createStore()
+            }, resolve);
+        }).then(() => this.state.store.dispatch(appWillMount(this)));
+    }
+
+    componentWillUnmount() {
+        this.state.store.dispatch(appWillUnmount(this));
+    }
+
+    render() {
+        const { route: { component, props }, store } = this.state;
+
+        if (store) {
+            return (
+                <Provider store = { store }>
+                    <Fragment>
+                        { this._createMainElement(component, props) }
+                    </Fragment>
+                </Provider>
+            )
+        }
+
+        return null;
+    }
+
+    _createMainElement(component, props) {
+        return component ? React.createElement(component, props || {}) : null;
+    }
+
+    /**
+     * 
+     * @returns store
+     */
+    _createStore() {
+        const reducer = ReducerRegistry.combineReducers();
+        let middleware = MiddlewareRegistry.applyMiddleware(Thunk);
+        const store = createStore(reducer, PersistenceRegistry.getPersistedState(), middleware);
+
+        StateListenerRegistry.subscribe(store);
+
+        return store;
+    }
+
+    _navigate(route) {
+        if (_.isEqual(route, this.state.route)) {
+            return new Promise.resolve();
+        }
+
+        return new Promise(resolve => {
+            this.setState({ route }, resolve);
+        });
+    }
+}
