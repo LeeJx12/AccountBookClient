423b8d8e51ccfc8c143a27415440e27da86780da
LeeJx2 <46637231+LeeJx12@users.noreply.github.com>
2022-05-09 11:25:05 +0900

    Chore: react-native/checkbox 추가
    @react-native-community/checkbox 추가

---
+++ src/login/components/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    layout: {
+        flex: 1,
+    },
+
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '20dp'
+    },
+
+    logo: {
+        width: '300dp',
+        height: 'auto',
+        margin: '20dp'
+    },
+
+
+});
