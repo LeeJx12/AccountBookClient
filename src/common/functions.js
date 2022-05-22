4cc0dbab4b2dfa3b194ef98d2d21f43460480778
LeeJx2 <46637231+LeeJx12@users.noreply.github.com>
2022-05-22 19:17:37 +0900

    Feat: 개인설정 프로필 추가
    사용자 이름, 시작일 추가

--- src/common/functions.js
+++ src/common/functions.js
@@ -26,3 +26,26 @@ export function showProgressLayer(dispatch) {
 export function hideProgressLayer(dispatch) {
     dispatch(hideProgress());
 }
+
+/**
+ * 'yyyy-MM-dd hh:mm:ss' format에 따라 날짜 형식 반환
+ * @param {*} date 
+ * @param {*} format 
+ */
+export function getDateStr(date, format) {
+    const year =    date.getFullYear();
+    const month =   ((date.getMonth() + 1) + '').padStart(2, '0'); 
+    const day =     (date.getDate() + '').padStart(2, '0'); 
+    const hour =    (date.getHours() + '').padStart(2, '0'); 
+    const minute =  (date.getMinutes() + '').padStart(2, '0'); 
+    const second =  (date.getSeconds() + '').padStart(2, '0'); 
+
+    format = format.replace('yyyy', year);
+    format = format.replace('MM', month);
+    format = format.replace('dd', day);
+    format = format.replace('hh', hour);
+    format = format.replace('mm', minute);
+    format = format.replace('ss', second);
+
+    return format;
+}
--- src/setting/components/SettingPane.js
+++ src/setting/components/SettingPane.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Container from "../../app/components/Container";
+import { getDateStr } from "../../common/functions";
 
 export class SettingPane extends Component {
     constructor(props) {
@@ -8,15 +9,16 @@ export class SettingPane extends Component {
     }
 
     render() {
+        const { _userName, _startDate } = this.props;
 
         return (
             <Container>
                 <div className="row py-3 justify-content-around align-items-center profile_layout">
                     <div className="col">
-                        <h4>이정준</h4>
-                        <small className="text-muted">2022.04.30</small>
+                        <h4>{_userName}</h4>
+                        <small className="text-muted">{_startDate}</small>
                     </div>
-                    <div className="col" style={{textAlign: 'end'}}>
+                    <div className="col">
                         <button type="button" className="btn btn-outline-primary btn-sm bg-white">로그아웃</button>
                     </div>
                 </div>
@@ -24,9 +26,10 @@ export class SettingPane extends Component {
                     <div className="col">
                         <div className="mb-3">
                             <label htmlFor="targetAmount" className="form-label">
-                                <img src="./icons/icon-target.svg" className="img-fluid pb-1" /> 목표액 </label>
+                                <img src="./icons/icon-target.svg" className="img-fluid pb-1" /> 목표액 
+                            </label>
                             <input type="text" className="form-control" id="targetAmount" placeholder="100,000,000" aria-describedby="amountHelper"/>
-                                <div id="amountHelper" className="form-text mx-2">1억원</div>
+                            <div id="amountHelper" className="form-text mx-2">1억원</div>
                         </div>
                     </div>
                 </div>
@@ -34,9 +37,10 @@ export class SettingPane extends Component {
                     <div className="col">
                         <div className="mb-3">
                             <label htmlFor="monthLife" className="form-label">
-                                <img src="./icons/icon-calendar-red.svg" className="img-fluid pb-1" /> 월 생활비 </label>
+                                <img src="./icons/icon-calendar-red.svg" className="img-fluid pb-1" /> 월 생활비 
+                            </label>
                             <input type="text" className="form-control" id="monthLife" placeholder="500,000" aria-describedby="monthHelper"/>
-                                <div id="monthHelper" className="form-text mx-2">50만원</div>
+                            <div id="monthHelper" className="form-text mx-2">50만원</div>
                         </div>
                     </div>
                 </div>
@@ -44,16 +48,18 @@ export class SettingPane extends Component {
                     <div className="col">
                         <div className="mb-3">
                             <label htmlFor="dayLife" className="form-label">
-                                <img src="./icons/icon-calc-blue.svg" className="img-fluid pb-1" /> 일일 생활비 </label>
+                                <img src="./icons/icon-calc-blue.svg" className="img-fluid pb-1" /> 일일 생활비 
+                            </label>
                             <input type="text" className="form-control" id="dayLife" placeholder="10,000" aria-describedby="dayHelper"/>
-                                <div id="dayHelper" className="form-text mx-2">1만원</div>
+                            <div id="dayHelper" className="form-text mx-2">1만원</div>
                         </div>
                     </div>
                 </div>
                 <div className="row pt-3">
                     <div className="col">
                         <label htmlFor="livexpdepositid" className="form-label">
-                            <img src="./icons/icon-card.svg" className="img-fluid pb-1" /> 생활비 계좌 </label>
+                            <img src="./icons/icon-card.svg" className="img-fluid pb-1" /> 생활비 계좌 
+                        </label>
                         <select className="form-select" id="livexpdepositid" aria-label="주 생활비 계좌를 등록하세요!">
                             <option value="1">신한</option>
                             <option value="2">카카오페이</option>
@@ -72,8 +78,13 @@ export class SettingPane extends Component {
 }
 
 function _mapStateToProps(state) {
-    return {
+    const sessionUser = state[`leejx2/accountbook/login`].sessionUser;
+    const { userName } = sessionUser;
+    const startDate = getDateStr(new Date(sessionUser.startDate), 'yyyy.MM.dd');
 
+    return {
+        _userName: userName,
+        _startDate: startDate,
     };
 }
 
