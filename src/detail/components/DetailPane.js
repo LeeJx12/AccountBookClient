0cb81b6ee63bc18cc0e21d4ff1f778028a35db9e
LeeJx2 <46637231+LeeJx12@users.noreply.github.com>
2022-06-02 15:04:33 +0900

    Design: inline-css 정리
    inline-css 정리

--- src/detail/components/DetailPane.js
+++ src/detail/components/DetailPane.js
@@ -18,15 +18,15 @@ export class DetailPane extends Component {
                     </div>
                     <div className="card-body">
                         <h6 className="card-title">주거래 통장</h6>
-                        <h6 className="card-subtitle mb-2 pb-3 text-muted" style={{borderBottom: '1px solid #e9ecef'}}>2022.05.01 ~ 2024.04.30</h6>
-                    <div className="row">
-                        <div className="col">
-                            <p className="card-text" style={{fontWeight: 'bold'}}>100,000,000 원</p>
+                        <h6 className="card-subtitle mb-2 pb-3 text-muted">2022.05.01 ~ 2024.04.30</h6>
+                        <div className="row">
+                            <div className="col">
+                                <p className="card-text">100,000,000 원</p>
+                            </div>
+                            <div className="col-2 mx-1 deposit-more">
+                                <img src="./icons/icon-more.svg" className="img-fluid"/>
+                            </div>
                         </div>
-                        <div className="col-2 mx-1" style={{textAlign: 'end'}}>
-                            <img src="./icons/icon-more.svg" className="img-fluid" style={{width: '20px'}}/>
-                        </div>
-                    </div>
                     </div>
                 </div>
                 <div className="card mt-3 mb-1">
@@ -35,13 +35,13 @@ export class DetailPane extends Component {
                     </div>
                     <div className="card-body">
                         <h6 className="card-title">생활비 통장</h6>
-                        <h6 className="card-subtitle mb-2 pb-3 text-muted" style={{borderBottom: '1px solid #e9ecef'}}>2022.05.01 ~ 2024.04.30</h6>
+                        <h6 className="card-subtitle mb-2 pb-3 text-muted">2022.05.01 ~ 2024.04.30</h6>
                     <div className="row">
                         <div className="col">
-                            <p className="card-text" style={{fontWeight: 'bold'}}>100,000,000 원</p>
+                            <p className="card-text">100,000,000 원</p>
                         </div>
-                        <div className="col-2 mx-1" style={{textAlign: 'end'}}>
-                            <img src="./icons/icon-more.svg" className="img-fluid" style={{width: '20px'}}/>
+                        <div className="col-2 mx-1">
+                            <img src="./icons/icon-more.svg" className="img-fluid"/>
                         </div>
                     </div>
                     </div>
@@ -52,20 +52,20 @@ export class DetailPane extends Component {
                     </div>
                     <div className="card-body">
                         <h6 className="card-title">과학기술인공제회 청년적금</h6>
-                        <h6 className="card-subtitle mb-2 pb-3 text-muted" style={{borderBottom: '1px solid #e9ecef'}}>2022.05.01 ~ 2024.04.30</h6>
+                        <h6 className="card-subtitle mb-2 pb-3 text-muted">2022.05.01 ~ 2024.04.30</h6>
                     <div className="row">
                         <div className="col">
-                            <p className="card-text" style={{fontWeight: 'bold'}}>100,000,000 원</p>
+                            <p className="card-text">100,000,000 원</p>
                         </div>
-                        <div className="col-2 mx-1" style={{textAlign: 'end'}}>
-                            <img src="./icons/icon-more.svg" className="img-fluid" style={{width: '20px'}}/>
+                        <div className="col-2 mx-1">
+                            <img src="./icons/icon-more.svg" className="img-fluid"/>
                         </div>
                     </div>
                     </div>
                 </div>
                 <div className="row my-4">
                     <div className="col" style={{textAlign: 'center'}}>
-                        <img src="./icons/icon-plus.svg" className="img-fluid" style={{width: '20px'}} />
+                        <img src="./icons/icon-plus.svg" className="img-fluid" />
                     </div>
                 </div>
             </Container>
